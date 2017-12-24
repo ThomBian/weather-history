c4f652dff725402023e3a6fc9ac7b787bab37cea
Thomas <thomas.bianchini1993@gmail.com>
2017-12-24 11:32:11 +0100

    [OK] Histogram built with d3 using mocked data

--- src/app/component/historgram/histogram.component.ts
+++ src/app/component/historgram/histogram.component.ts
@@ -11,7 +11,6 @@ const data:any = {
     "18":16,
     "20":12,
     "22":8,
-    "23":-1
 }
 
 const test:number [] = [
@@ -19,7 +18,7 @@ const test:number [] = [
 ]
 
 const xScale = d3.scaleLinear();
-const yScale = d3.scaleLinear().domain([0, d3.max(test)]).range([0, 340]);
+const yScale = d3.scaleLinear().range([0, 340]);
 
 
 @Component({
@@ -30,17 +29,80 @@ const yScale = d3.scaleLinear().domain([0, d3.max(test)]).range([0, 340]);
 })
 export class Histogram implements OnInit{
 
+    margin = {
+        top: 20,
+        bottom: 90,
+        right: 10,
+        left: 10
+    }
+    width:number = 600 - this.margin.left - this.margin.right;
+    height:number = 400 - this.margin.top - this.margin.bottom;
+    chart:any;
 
+    data:{} = data;
+    values: number[];
+    keys: any[];
+    
+    constructor() {
+        this.values = Object.values(this.data).map(Number);
+        this.keys = Object.keys(this.data);
+    }
+    
+    createHistrogram() {
+
+        let yScale = d3.scaleLinear()
+            .domain([0, d3.max(this.values)])
+            .range([this.height, 0]); // [height, 0] due to top left corner reference in svg
+    
+        let xScale = d3.scaleBand()
+            .domain(this.keys)
+            .rangeRound([0, this.width]);
+
+        this.chart = d3.select('#root')
+            .attr('width', this.width + this.margin.left + this.margin.right)
+            .attr('height', this.height + this.margin.top + this.margin.bottom)
+            .append('g')
+                .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
         
+        this.createBars(xScale, yScale);  
+        this.createAxis(xScale, yScale);   
+    }
+
+    createBars(xScale, yScale){
+        let barContainer = this.chart.selectAll('g')
+                    .data(this.values)
+                    .enter()
+                    .append('g')
+                        .attr('class', 'bar')
+                        .attr('transform', (d, i) => `translate(${i * xScale.bandwidth()}, 0)`);
+        
+        barContainer.append('rect')
+            .attr('y', d => yScale(d))
+            .attr('height', d => this.height - yScale(d))
+            .attr('width', xScale.bandwidth() - 2);
+        
+        barContainer.append('text')
+            .attr('x', xScale.bandwidth()/2 - 14)
+            .attr('y', d => this.height - 10)
+            .text(d => `${d} ˚C`);
+    }
+
+    createAxis(xScale, yScale) {
+        const axisX = d3.axisBottom(xScale);
+        const axisY = d3.axisLeft(yScale);
+
+        this.chart.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', `translate(0, ${this.height})`)
+            .call(axisX)
+            .append('text')
+            .attr('class', 'legend')
+            .attr('x', this.width / 2)
+            .attr('y', 50)
+            .text('Time of day')
+    }
 
     ngOnInit() {
-        d3.select('#root')
-            .selectAll('div')
-            .data(test)
-                 .enter()
-                 .append('div')
-                 .attr('class', 'bar')
-                 .style('height', d => `${yScale(d)}px`)
-                 .text(d => d+'');                     
+        this.createHistrogram();     
     }
 }
--- src/app/component/search-city/search-city.component.ts
+++ src/app/component/search-city/search-city.component.ts
@@ -30,6 +30,7 @@ export class SearchCity {
   }
 
   onSelect(city:CityW) {
+    this.foundedCities = [];
     this.selectedCity = city;
   }
 }
